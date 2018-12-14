4585d901e2fef63a2740b2ee59042efe4cc6cf5d
Seong-Hwan Jun <s2jun.uw@gmail.com>
2018-12-14 19:29:58 +0100

    code for a2q2 supplemented

--- a2/q2/q2.py
+++ a2/q2/q2.py
@@ -2,61 +2,57 @@ from q2generator import *
 import numpy as np
 import pdb
 
-def conditional_likelihood(G, start):
+def conditional_likelihood(o, G, start):
 	pass
 
 # o: observations
 # n_lattice: size of the lattice
 # num_iter: number of MCMC iterations
-def mh_w_gibbs(o, n_lattice, num_iter):
-	G = initialize_graph(n_lattice)
+def mh_w_gibbs(o, G, num_iter):
 	s = [] # store samples for the start positions
 	X = [] # store switch states
-	X.append(G) # initial sample
-	s.append(G[0][0]) # set the initial start position as the one at G[0][0]
+	X.append(sample_switch_states(G.lattice_size)) # generate initial switch state
+	s.append(sample_start_pos(G)) # set the initial start position as the one at G[0][0]
 	for n in range(num_iter):
 		pass
 	return s, X
 
-def gibbs(o, n_lattice, num_iter):
-	G = initialize_graph(n_lattice)
+def gibbs(o, G, num_iter):
 	s = [] # store samples for the start positions
 	X = [] # store switch states
-	X.append(G) # initial sample
-	s.append(G[0][0]) # set the initial start position as the one at G[0][0]
+	X.append(sample_switch_states(G.lattice_size)) # generate initial switch state
+	s.append(sample_start_pos(G)) # set the initial start position as the one at G[0][0]
 	for n in range(num_iter):
-
 		pass
 	return s, X
 
-def block_gibbs(o, n_lattice, num_iter):
-	G = initialize_graph(n_lattice)
+def block_gibbs(o, G, num_iter):
 	s = [] # store samples for the start positions
 	X = [] # store switch states
-	X.append(G) # initial sample
-	s.append(G[0][0]) # set the initial start position as the one at G[0][0]
+	X.append(sample_switch_states(G.lattice_size)) # generate initial switch state
+	s.append(sample_start_pos(G)) # set the initial start position as the one at G[0][0]
 	for n in range(num_iter):
 		pass
 	return s, X
 
 # generate sample graph and observations
 def main():
-	seed = 2
+	seed = 17
 	n_lattice = 3
 	T = 100
 	p = 0.1
-	G_truth, s_truth, o = generate_data(seed, T, n_lattice, p)
+	X_truth, s_truth, o = generate_data(seed, n_lattice, T, p)
 	print(o)
-	print(G_truth)
-	print("state:")
-	print(s_truth[0:6])
+	print(X_truth)
+	print(s_truth[1:20])
 
-	# discard G and s 
-	# infer s[0] and switch states given o
+	# randomize the switch states -- get initial state
+	G_0 = TrainLattice(n_lattice)
+	X = sample_switch_states(n_lattice)
+
+	# infer s[0] and switch states given o and G
 	num_iter = 1000
-	s, X = mh_w_gibbs(o, n_lattice, num_iter)
-	s, X = gibbs(o, n_lattice, num_iter)
-	s, X = block_gibbs(o, n_lattice, num_iter)
+	s, X = mh_w_gibbs(o, G_0, num_iter)
 	
 	# YOUR CODE:
 	# analyze s, X by comparinson to the ground truth in s[0] and G
--- a2/q2/q2generator.py
+++ a2/q2/q2generator.py
@@ -1,5 +1,7 @@
 import numpy as np
 import pdb
+import copy
+import sys
 
 def validate_idx(row, col, n):
 	r = row
@@ -14,93 +16,112 @@ def validate_idx(row, col, n):
 		c = 0
 	return (r, c)
 
-class Switch:
-	n = 0
-	def __init__(self, row, col, state):
+class TrainLattice:
+	def __init__(self, lattice_size):
+		self.G = []
+		self.lattice_size = lattice_size
+		for r in range(self.lattice_size):
+			row = []
+			for c in range(self.lattice_size):
+				node = Node(r, c)
+				row.append(node)
+			self.G.append(row)
+
+	def get_node(self, r, c):
+		return self.G[r][c]
+
+	# returns next node
+	def get_next_node(self, curr_node, entry_dir, X):
+		exit_direction = 0 # if train entered curr_node through dir {1, 2, 3}, it exists through direction 0
+		if entry_dir == 0: # otherwise, follows the current state of the switch
+			exit_direction = X[curr_node.row][curr_node.col]
+		(i, j) = curr_node.edges[exit_direction]
+		(row, col) =  validate_idx(curr_node.row + i, curr_node.col + j, self.lattice_size)		
+		return (self.get_node(row, col), exit_direction)
+
+	def get_entry_direction(self, prev_pos, curr_pos):
+		for direction in range(4):
+			(i, j) = curr_pos.get_edge(direction)
+			(r0, c0) = validate_idx(curr_pos.row + i, curr_pos.col + j, self.lattice_size)
+			if r0 == prev_pos.row and c0 == prev_pos.col:
+				return direction
+		print("Bug")
+		sys.exit(-1)
+
+class Node:
+	def __init__(self, row, col):
 		self.row = row
 		self.col = col
-		self.state = state
 		self.sample_edge_labels()
 
 	def sample_edge_labels(self):
-		self.edges = [(-1, 0), (0, -1), (1, 0), (0, 1)]
-		np.random.shuffle(self.edges)
+		self.edges = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+		np.random.shuffle(self.edges) # shuffling the edges is equivalent to randomly assigning edge labels for this node
 
-	def equals_0_dir(self, e):
-		(i, j) = self.get_0_dir()
-		if i == e.row and j == e.col:
-			return True
-		return False
-
-	def get_0_dir(self):
-		i, j = self.edges[0]
-		return validate_idx(self.row + i, self.col + j, Switch.n)
+	def get_edge(self, i):
+		return self.edges[i]
 
 	def __repr__(self):
 		ret = "\npos: " + str(self.row) + ", " + str(self.col) + "\n"
-		ret += "state: " + str(self.state) + "\n"
 		for i in range(len(self.edges)):
 			ret += str(i) + ": " + str(self.edges[i]) + "\n"
 		return ret
 
+# return random edge direction with error_prob or d[0] with (1 - error_prob)
+def generate_single_obs(pos, edge_dir, p):
+	u = np.random.uniform(0, 1, 1)[0]
+	#pdb.set_trace()
+	if u < p:
+		print("Generating errorneous observation!")
+		return np.random.randint(0, 4, 1)[0]
+	return edge_dir
+
+# s0: starting position
+# X: switch state (as list of list)
+# G: object of type TrainLattice
+# error_prob: probability of observation error
+# length: how many observations to generate
+def simulate(s0, X, G, length, error_prob):
+	s = [s0] # starting position
+	o = []
+	entry_directions = [0] # the train enters s_0 from edge direction 0
+	for t in range(1, length):
+		#pdb.set_trace()
+		(s_t, exit_dir) = G.get_next_node(s[t-1], entry_directions[t-1], X)
+		o_t = generate_single_obs(s_t, exit_dir, error_prob)
+		entry_dir = G.get_entry_direction(s[t-1], s_t)
+		s.append(s_t)
+		entry_directions.append(entry_dir)
+		o.append(o_t)
+		#print(s[t-1], exit_dir)
+		#print(s_t, entry_dir)
+	return s, o
+
+# returns a node selected at random
+def sample_start_pos(G):
+	r = np.random.randint(0, G.lattice_size)
+	c = np.random.randint(0, G.lattice_size)
+	s0 = G.get_node(r, c)
+	return s0
+
 # generate graph and switch states
-# n >= 2
-def initialize_graph(n = 2):
+# returns nxn matrix as list of list
+def sample_switch_states(n):
 	X = []
-	for i in range(n):
+	for r in range(n):
 		row = []
-		for j in range(n):
+		for c in range(n):
 			state = np.random.randint(low=1, high=4, size=1)[0]
-			row.append(Switch(i, j, state))
+			row.append(state)
 		X.append(row)
 	return X
 
-def generate_single_obs(prev, curr, p):
-	true_state = curr.state
-	u = np.random.uniform(0, 1, 1)[0]
-	if u < p:
-		#print("return random state")
-		return np.random.randint(0, 4, 1)[0]
-	#print("return true state")
-	if prev == None or curr.equals_0_dir(prev):
-		return true_state
-	else:
-		return 0
-
-def advance_train(prev, curr, p):
-	if prev is None or curr.equals_0_dir(prev):
-		(i, j) =  curr.edges[curr.state]
-		(row, col) =  validate_idx(curr.row + i, curr.col + j, Switch.n)
-	else:
-		(row, col) =  curr.get_0_dir()
-	return row, col
-
-# sample starting position
-def simulate(X, T, p):
-	n = len(X)
-	row = np.random.randint(0, n, 1)[0]
-	col = np.random.randint(0, n, 1)[0]
-	s = []
-	o = []
-
-	prev = None
-	curr = X[row][col]
-	s.append(curr)
-	o.append(generate_single_obs(prev, curr, p))
-	#print("t: ", 0, "\nobs: " + str(o[0]), "\ncurr[", s[0], "]\n")
-	for t in range(1, T):
-		row, col = advance_train(prev, curr, p)
-		prev = curr
-		curr = X[row][col]
-		obs = generate_single_obs(prev, curr, p)
-		s.append(curr)
-		o.append(obs)
-		#print("t: ", (t), "\nobs: " + str(o[t]), "\ncurr[", s[t], "]\n")
-	return s, o
-
-def generate_data(seed, T, n, p):
+def generate_data(seed, n_lattice, length, error_prob):
 	np.random.seed(seed)
-	Switch.n = n
-	X = initialize_graph(n)
-	s, o = simulate(X, T, p)
-	return X, s, o
+	G = TrainLattice(n_lattice)
+	r = np.random.randint(0, n_lattice)
+	c = np.random.randint(0, n_lattice)
+	s0 = G.get_node(r, c)
+	X = sample_switch_states(n_lattice)
+	positions, observations = simulate(s0, X, G, length, error_prob)
+	return X, positions, observations
